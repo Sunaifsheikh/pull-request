bcb3db0973f373c0f1016008b0f2a58c9c053e8e
TalhaNaveed786-hub <81877235+TalhaNaveed786-hub@users.noreply.github.com>
2021-11-27 22:32:43 +0500

    Add files via upload

---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+// import App from "../App";
+
+// const Signup =()=>{
+//     function onSubmitHandler (event) {
+//         event.preventDefault
+//     }
+//     return (
+//         <h1>My App</h1>
+//     )
+// }
+
+// export default Signup ();
